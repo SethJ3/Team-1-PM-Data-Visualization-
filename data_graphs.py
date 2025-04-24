f5db0f3d1a84a311ae749a52f04e889bf5e796f8
SethJ3 <seth.johns@fwcsstudents.org>
2025-04-24 11:41:01 -0400

    Add files via upload

---
+++ data_graphs.py
@@ -0,0 +1,32 @@
+import csv
+from pathlib import Path
+from plotly.subplots import make_subplots
+
+#highs is daily high, lows is daily lows, prcp is daily rain
+
+path = Path('sandusky_weather.csv')
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+targeted_dates = ["18", "19", "20", "21", "22"]
+highs, lows, prcp = [], [], []
+for row in reader:
+        date = (row[header_row.index("DATE")])
+        if "2012" in date and "-07-" in date[4:] and date[8:] in targeted_dates\
+                and row[header_row.index("STATION")] == "USW00014846":
+            high = (row[header_row.index("TMAX")])
+            low = (row[header_row.index("TMIN")])
+            rain = (row[header_row.index("PRCP")])
+            highs.append(high)
+            lows.append(low)
+            prcp.append(rain)
+
+
+#y values set as y values fix
+fig = make_subplots(
+    rows=1, cols=3,
+    subplot_titles=("Highs and lows", "Rain", "Wind speed and Direction"), shared_yaxes=True
+)
+fig.add_bar(x=targeted_dates, y=[highs,lows], row=1, col=1)
+fig.show()
